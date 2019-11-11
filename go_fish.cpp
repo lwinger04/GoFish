5b47cb391c087b61461b157228d4df755088b734
lwinger04 <47575851+lwinger04@users.noreply.github.com>
2019-11-11 12:48:34 -0600

    Add files via upload

--- go_fish.cpp
+++ go_fish.cpp
@@ -7,8 +7,12 @@
 #include <cstdlib>
 #include "deck.h"
 #include "player.h"
+#include <fstream>
+
 using namespace std;
 int main(){
+    ofstream myfile("gofish_results.txt");
+    if(myfile.is_open()){
     Card c1;
     Card c2;
     Deck d;
@@ -17,99 +21,101 @@ int main(){
     Player p1("Bob");
     Player p2("Peter");
 
-    cout << "Player 1 is: " << p1.getName() << endl;
-    cout << "Player 2 is: " << p2.getName() << endl;
+    myfile << "Player 1 is: " << p1.getName() << endl;
+    myfile << "Player 2 is: " << p2.getName() << endl;
     for(int i = 0; i<7; i++){
         p1.addCard(d.dealCard());
         p2.addCard(d.dealCard());
     }
-    cout << p1.getName() << "'s hand: "<< p1.showHand() << endl;
-    cout << p2.getName() << "'s hand: "<< p2.showHand() << endl;
+    myfile << p1.getName() << "'s hand: "<< p1.showHand() << endl;
+    myfile << p2.getName() << "'s hand: "<< p2.showHand() << endl;
     while ((p1.getHandSize() != 0)&&(p1.checkHandForBook(c1, c2))) {
         p1.bookCards(c1, c2);
     }
-    cout << p1.getName() << "'s books: " << p1.showBooks() << endl;
+    myfile << p1.getName() << "'s books: " << p1.showBooks() << endl;
     while ((p2.getHandSize() != 0)&&(p2.checkHandForBook(c1, c2))) {
         p2.bookCards(c1, c2);
     }
-    cout << p2.getName() << "'s books: " << p2.showBooks() << endl;
+    myfile << p2.getName() << "'s books: " << p2.showBooks() << endl;
 
     while(!(p1.getHandSize() == 0 && p2.getHandSize() == 0 && d.size() == 0)) {
-        cout << "\n" << p1.getName() << "'s turn" << endl;
-		if(p1.getHandSize()!=0) {
+        myfile << "\n" << p1.getName() << "'s turn" << endl;
+        if(p1.getHandSize()!=0) {
             c1 = p1.chooseCardFromHand();
-            cout << p1.getName() << ": Do you have a " << c1.rankString(c1.getRank()) << "?" << endl;
+            myfile << p1.getName() << ": Do you have a " << c1.rankString(c1.getRank()) << "?" << endl;
             if (p2.cardInHand(c1)) {
-                cout << p2.getName() << ": Yes" << endl;
+                myfile << p2.getName() << ": Yes" << endl;
                 c2 = p2.removeCardFromHand(c1);
                 p1.addCard(c2);
                 while ((p1.getHandSize() != 0)&&(p1.checkHandForBook(c1, c2))) {
                     p1.bookCards(c1, c2);
                 }
-                cout << p1.getName() << "'s books: " << p1.showBooks() << endl;
-				cout << p1.getName() << "'s hand: " << p1.showHand() << endl;
+                myfile << p1.getName() << "'s books: " << p1.showBooks() << endl;
+                myfile << p1.getName() << "'s hand: " << p1.showHand() << endl;
             } else {
-                cout << p2.getName() << ": No" << endl;
+                myfile << p2.getName() << ": No" << endl;
                 p1.addCard((d.dealCard()));
-                cout << p1.getName() << "'s hand: " << p1.showHand() << endl;
+                myfile << p1.getName() << "'s hand: " << p1.showHand() << endl;
                 while ((p1.getHandSize() != 0)&&(p1.checkHandForBook(c1, c2))) {
                     p1.bookCards(c1, c2);
                 }
             }
-            cout << p2.getName() << "'s hand: " << p2.showHand() << endl;
+            myfile << p2.getName() << "'s hand: " << p2.showHand() << endl;
         }
         else {
             if (d.size() != 0) {
                 p1.addCard((d.dealCard()));
-                cout << p1.getName() << "'s hand: " << p1.showHand() << endl;
-                cout << p1.getName() << "'s books: " << p1.showBooks() << endl;
+                myfile << p1.getName() << "'s hand: " << p1.showHand() << endl;
+                myfile << p1.getName() << "'s books: " << p1.showBooks() << endl;
             }
         }
-   
-		if(!(p1.getHandSize() == 0 && p2.getHandSize() == 0 && d.size() == 0)) {
-	        cout << "\n" << p2.getName() << "'s turn" << endl;
-		}
 
-		if(p2.getHandSize()!=0) {
+        if(!(p1.getHandSize() == 0 && p2.getHandSize() == 0 && d.size() == 0)) {
+            myfile << "\n" << p2.getName() << "'s turn" << endl;
+        }
+
+        if(p2.getHandSize()!=0) {
             c1 = p2.chooseCardFromHand();
-            cout << p2.getName() << ": Do you have a " << c1.rankString(c1.getRank()) << "?" << endl;
+            myfile << p2.getName() << ": Do you have a " << c1.rankString(c1.getRank()) << "?" << endl;
             if (p1.cardInHand(c1)) {
-                cout << p1.getName() << ": Yes" << endl;
+                myfile << p1.getName() << ": Yes" << endl;
                 c2 = p1.removeCardFromHand(c1);
                 p2.addCard(c2);
                 while ((p2.getHandSize() != 0)&&(p2.checkHandForBook(c1, c2))) {
                     p2.bookCards(c1, c2);
                 }
-                cout << p2.getName() << "'s books: " << p2.showBooks() << endl;
-				cout << p2.getName() << "'s hand: " << p2.showHand() << endl;
+                myfile << p2.getName() << "'s books: " << p2.showBooks() << endl;
+                myfile << p2.getName() << "'s hand: " << p2.showHand() << endl;
             } else {
-                cout << p1.getName() << ": No" << endl;
+                myfile << p1.getName() << ": No" << endl;
                 p2.addCard((d.dealCard()));
-                cout << p2.getName() << "'s hand: " << p2.showHand() << endl;
+                myfile << p2.getName() << "'s hand: " << p2.showHand() << endl;
                 while ((p2.getHandSize() != 0)&&(p2.checkHandForBook(c1, c2))) {
                     p2.bookCards(c1, c2);
                 }
             }
-            cout << p1.getName() << "'s hand: " << p1.showHand() << endl;
+            myfile << p1.getName() << "'s hand: " << p1.showHand() << endl;
         }
         else{
             if (d.size() != 0) {
                 p2.addCard((d.dealCard()));
-                cout << p2.getName() << "'s hand: " << p2.showHand() << endl;
-                cout << p2.getName() << "'s books: " << p2.showBooks() << endl;
+                myfile << p2.getName() << "'s hand: " << p2.showHand() << endl;
+                myfile << p2.getName() << "'s books: " << p2.showBooks() << endl;
             }
         }
     }
-	cout << "\n" << endl;
-	cout << p1.getName() << "'s books: " << p1.showBooks() << endl;
-	cout << p2.getName() << "'s books: " << p2.showBooks() << endl;
-	if (p1.getBookSize() > p2.getBookSize()) {
-		cout << p1.getName() << " wins!" << endl;
-	} else if (p2.getBookSize() > p1.getBookSize()) {
-		cout << p2.getName() << " wins!" << endl;
-	} else {
-		cout << "It's a draw!" << endl;
-	}	
-
+    myfile << "\n" << endl;
+    myfile << p1.getName() << "'s books: " << p1.showBooks() << endl;
+    myfile << p2.getName() << "'s books: " << p2.showBooks() << endl;
+    if (p1.getBookSize() > p2.getBookSize()) {
+        myfile << p1.getName() << " wins!" << endl;
+    } else if (p2.getBookSize() > p1.getBookSize()) {
+        myfile << p2.getName() << " wins!" << endl;
+    } else {
+        myfile << "It's a draw!" << endl;
+    }
+      myfile.close();
+   }
+   else cout << "Unable to open file";
     return(EXIT_SUCCESS);
 }
--- readme.txt
+++ readme.txt
@@ -1,4 +1,10 @@
-Type 'make' to compile project
+Need to run 'module load gcc'
+Then, type 'make' to compile the project
 Type './test' to run
 Player 1 is Bob, Player 2 is Peter
-If a player receives a card that they ask for, they book the cards and their turn is over
+Both players start with 7 cards in their hand
+Books are defined as 2 cards of the same rank
+Each player is only allowed one action per turn 
+	i.e. can get a card from a player or the deck but cannot ask for another card even if a book is formed.
+Game ends when the deck and hands are empty
+Player with the most books wins.
